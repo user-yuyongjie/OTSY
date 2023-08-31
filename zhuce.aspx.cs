872c8963c703f77d0b2cbee2889741a3e8f30451
user-yuyongjie <113125123+user-yuyongjie@users.noreply.github.com>
2023-08-31 22:16:04 +0800

    Add files via upload

---
+++ zhuce.aspx.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Collections;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using System.Data.SqlClient;
+
+public partial class zhuce : System.Web.UI.Page
+{
+    Datacon dataconn = new Datacon();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            //调用公共类中的ecDropDownList方法
+            dataconn.ecDropDownList(ddlProfession, "select * from tb_Profession", "Name", "ID");
+            Label4.Visible = false;
+            Label2.Visible = false;
+            Label3.Visible = false;
+        }
+    }
+
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        txtStuID.Text = "";
+        txtStuName.Text = "";
+        txtStuPwd.Text = "";
+        txtStuFPwd.Text = "";
+        txtQuePwd.Text = "";
+        txtAnsPwd.Text = "";
+        txtStuID.Focus();
+    }
+    protected void Button3_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("Default.aspx");
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        //调用公共类中的eccom，执行SQL语句命令
+        dataconn.eccom("insert into tb_Student"
+            + "(ID,Name,PWD,question,answer,Sex,profession)"
+            + "values('" + this.txtStuID.Text + "','" + this.txtStuName.Text + "','"
+            + this.txtStuPwd.Text + "','" + this.txtQuePwd.Text + "','"
+            + this.txtAnsPwd.Text + "','" + this.ddlSex.Text + "',"
+            + Convert.ToInt32(ddlProfession.SelectedValue) + ")");
+             //如果添加成功，弹出成功对话框
+            Label4.Visible = true;
+    }
+    protected void Button1_Click1(object sender, EventArgs e)
+    {
+        SqlDataReader read = dataconn.ExceRead("select * from tb_Student where ID='" + this.txtStuID.Text + "'");
+		read.Read();
+		if (read.HasRows)
+		{
+            if (this.txtStuID.Text == read["ID"].ToString())
+			{
+
+                Label2.Visible = true;
+			}
+		}
+		else
+		{
+            Label3.Visible = true;
+		}
+		read.Close();
+    }
+}
